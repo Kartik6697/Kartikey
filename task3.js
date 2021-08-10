852e255fa661c49212f0c992df5dcc9a6ed6a256
Kartikey Dave <kartikey.dave@green-apex.com>
2021-08-10 13:06:16 +0530

    changes in 09aug tasks

--- task3.js
+++ task3.js
@@ -1,7 +1,6 @@
 // loop created for get 16 input values in sequence of 0-15
 for (let x = 0; x <= 15; x++) {
-  
-    // check the condition if value is 0 or reminder of value is 0 then print the statement
+  // check the condition if value is 0 or reminder of value is 0 then print the statement
   if (x === 0 || x % 2 === 0) {
     document.write(x + " is even" + "<br>");
   }
