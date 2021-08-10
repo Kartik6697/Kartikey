dd7c6937d51f6fb29f5d93df699700a87071657e
Kartikey Dave <kartikey.dave@green-apex.com>
2021-08-10 14:34:07 +0530

    changes in 09aug tasks

--- task4.js
+++ task4.js
@@ -7,23 +7,26 @@ let chicken, dogs, cows;
 
 // get value of chickens and count their legs:
 x = prompt("enter number of chickens :", x);
-document.write("no. of chickens : ", x + "<br>");
+console.log("no. of chickens : ", x);
 
 // get value of dogs and count their legs:
 y = prompt("enter number of dogs : ", y);
-document.write("no. of dogs : ", y + "<br>");
+console.log("no. of dogs : ", y);
 
 // get value of cows and count their legs:
 z = prompt("enter number of cows : ", z);
-document.write("no. of cows : ", z + "<br>");
+console.log("no. of cows : ", z);
 
 // count total no. of animals:
 total_animal = Number(x) + Number(y) + Number(z);
-document.write("Total number of Species : ", total_animal + "<br>");
+console.log("Total number of Species : ", total_animal);
 
 // condition to check that total no. of animals are >10 or not
 // if animals are >10 than process all the counting actions
-if (total_animal > 10) {
+if (total_animal >= 10) {
+  // displaying from which condition it is
+  document.write("total no. of animals >= 10 :" + "<br>");
+
   // counts legs and store values
   // used ES6 syntax of "templet string"
   C_legs = 2 * Number(x);
@@ -43,4 +46,29 @@ if (total_animal > 10) {
   // counts total no. of legs and print them on screen
   total_legs = Number(C_legs) + Number(D_legs) + Number(CW_legs);
   document.write("Total number of legs : ", total_legs + "<br>");
+} else if (total_animal < 10) {
+  // displaying from which condition it is
+  document.write("total no. of animals < 10 :" + "<br>");
+
+  // counts legs and store values
+  // used ES6 syntax of "templet string"
+  C_legs = 2 * Number(x);
+  chicken = ` ${Number(C_legs)} legs`;
+
+  D_legs = 4 * Number(y);
+  dogs = `  ${Number(D_legs)} legs`;
+
+  CW_legs = 4 * Number(z);
+  cows = ` ${Number(CW_legs)} legs`;
+
+  // this will print the no of species
+  document.write("chicken : ", chicken + "<br>");
+  document.write("dogs : ", dogs + "<br>");
+  document.write("cows : ", cows + "<br>");
+
+  // counts total no. of legs and print them on screen
+  total_legs = Number(C_legs) + Number(D_legs) + Number(CW_legs);
+  document.write("Total number of legs : ", total_legs + "<br>");
+} else {
+  document.write("enter valid input !!");
 }
