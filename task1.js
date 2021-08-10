ebfaed48aaf48af05be4960b81455f72973a0a72
Kartikey Dave <kartikey.dave@green-apex.com>
2021-08-10 13:54:41 +0530

    changes in 09aug tasks

--- task1.js
+++ task1.js
@@ -1,27 +1,22 @@
+// declaring variable and printing them
 let a;
 let result;
 a = prompt("enter value", +a);
-console.log("value a = ", a);
 document.write("a = ", a + "<br>");
-let b = 25;
+const b = 25;
 document.write("b = ", b + "<br>");
-// const b = prompt("enter value", +b);
-console.log("value b = ", b);
 
 //addition of two numbers
 result = Number(a) + Number(b);
 document.write("Adition of numbers : ", result + "<br>");
-// document.write("\n");
 
 //subtraction of two numbers
 result = Number(a) - Number(b);
 document.write("Subtraction of numbers : ", result + "<br>");
-// document.write("\n");
 
 //multiplication of two numbers
 result = Number(a) * Number(b);
 document.write("Multiplication of numbers :", result + "<br>");
-// document.write("\n");
 
 // division of two numbers round of 2 digits after "."
 result = Number(a) / Number(b);
@@ -33,7 +28,7 @@ result = Number(a) % Number(b);
 document.write("Reminder of numbers :", result + "<br>");
 
 //exponentiation of digit
-result = Number(a) ** 2 ;
+result = Number(a) ** 2;
 document.write("Exponential of numbers :", result + "<br>");
 
 //increment of digit
