ad7f0454efbcf8166571cf895308d7a523ff653d
Kartikey Dave <kartikey.dave@green-apex.com>
2021-08-17 19:41:52 +0530

    task3 javascript

--- tipcal.js
+++ tipcal.js
@@ -1,31 +1,26 @@
-let bill = [];
-//let tip = [];
+// declaration of an array for individual value storage
+let bill = [125, 555, 44];
+let tip = [];
 let total = [];
-let i, j;
-let tip;
+// let i, j;
+// let billAmt;
 
-calTip = () => {
-  for (i = 0; i < 3; i++) {
-    bill[i] = prompt('Enter your bill amount : '); // Note 2
-  }
+// function to calculate tip
+let calTip = bill => {
+  bill.map((Element, index) => {
+    //checking condition for 15% or 20% tip
+    if (Element <= 300 && Element >= 50) {
+      tip[index] = Element * 0.15;
+    } else {
+      tip[index] = Element * 0.2;
+    }
+    // get total bill amount
+    total[index] = tip[index] + bill[index];
+  });
 };
-calTip();
-document.getElementById('app').innerHTML = `bill amount are : ${bill}`;
-
-tipcalculate = item => {
-  console.log(
-    'Total bill amount with the tip amount is : ',
-    bill <= 300 && bill >= 50 ? (tip = bill * 0.15) : (tip = bill * 0.2)
-  );
-
-  // item.map(
-  //   (j,index) => (tip[j] += (bill[i]<=300 && bill[i]>=50) ? tip[j]=bill[i]*0.15: tip[j]=bill[i]*0.2 ))
-
-  // // for (j = 0; j < 3; j++) {
-  // //   tip[j] = prompt('Enter your tip amount : '); // Note 2
-  // // }
-  // const myMap = new Map(tip)
-  //  myMap.get((bill[i]<=300 && bill[i]>=50) ? tip[j]=bill[i]*0.15: tip[j]=bill[i]*0.2 );
-};
-tipcalculate();
-document.getElementById('app').innerHTML = `tip3 amount are : ${tip}`;
+//function called
+calTip(bill);
+// display values
+document.write(`bill amount : ${bill} <br>`);
+document.write(`tip for each bill amount : ${tip} <br>`);
+document.write(`total bill amount ${total} <br>`);
