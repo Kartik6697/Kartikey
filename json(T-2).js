d89da6b3538f204918f483a38432cefc4c217cbf
Kartikey Dave <kartikey.dave@green-apex.com>
2021-08-20 14:20:32 +0530

    updated

--- json(T-2).js
+++ json(T-2).js
@@ -1,62 +1,80 @@
 //for loop
-const jsondata = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
-for (let i = 0; i < jsondata.length; i++) {
-    document.write(`array's ${i} index values is : ${jsondata[i]} <br>` );
+document.write(`<h2> For loop </h2>`);
+const jsondata1 = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
+for (let i = 0; i < jsondata1.length; i++) {
+  document.write(`array's ${i} index values is : ${(1)[i]} <br>`);
 }
+document.write(`<br>`);
 
-// //forEach loop
-// const jsondata = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
-// jsondata.forEach(function1);
-// //create a function to display value
-// function function1(Value, index) {
-//    document.write(`index is: ${index} ==> Value is: ${Value} <br>`);
-// }
+//forEach loop
+document.write(`<h2> forEach loop </h2>`);
+const jsondata2 = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
+//create a function to display value
+const function1 = (Value, index) => {
+  document.write(`index is: ${index} ==> Value is: ${Value} <br>`);
+};
+jsondata2.forEach(function1);
+document.write(`<br>`);
 
-// //for_in loop
-// let jsondata = [
-//   { day: "Sunday", sport: "Hockey" },
-//   { day: "Saturday", sport: "Football" },
-// ];
+//for_in loop
+document.write(`<h2> for_in loop </h2>`);
+let jsondata3 = [
+  { day: "Sunday", sport: "Hockey" },
+  { day: "Saturday", sport: "Football" },
+];
+//for access jsondata array
+for (let key in jsondata3) {
+  //for access jsondata array value
+  for (let key1 in jsondata3[key]) {
+    document.write(`${jsondata3[key][key1]} <br>`);
+  }
+}
+document.write(`<br>`);
 
-// //for access jsondata array
-// for (let key in jsondata) {
-//   //for access jsondata array value
-//   for (let key1 in jsondata[key]) {
-//     document.write(`${jsondata[key][key1]} <br>`);
-//   }
-// }
+// for_of loop
+document.write(`<h2> for_of loop </h2>`);
+const jsondata4 = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
+let a,
+  i = 0;
+for (a of jsondata4) {
+  document.write(`${i++} index jsondata value : ${a}  <br >`);
+}
+document.write(`<br>`);
 
-// // for_of loop
-// const jsondata = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
-// let a , i=0;
+//while loop
+document.write(`<h2> while loop </h2>`);
+const myFunction = () => {
+  const jsondata5 = [
+    "Hockey",
+    "Badminton",
+    "Volleyball",
+    "Cricket",
+    "Football",
+  ];
+  let i = 0,
+    sports = ""; // one container declared named sports for store value of array in it
+  while (i < jsondata5.length) {
+    sports += jsondata5[i] + "<br>";
+    i++;
+  }
+  document.write(sports);
+};
+myFunction();
+document.write(`<br>`);
 
-// for (a of jsondata) {
-//   document.write(`${i++} index jsondata value : ${a}  <br >`);
-// }
-
-// //while loop
-// myFunction = () => {
-//     const jsondata = ["Hockey", "Badminton", "Volleyball", "Cricket", "Football"];
-//     let i=0, sports = "";   // one container declared named sports for store value of array in it
-//     while (i < jsondata.length) {
-//         sports += jsondata[i] + "<br>";
-//         i++;
-//     }
-//     document.write(sports);
-//   }
-//   myFunction();
-
-//map implementation
-// let data;
-// const jsondata = [
-//   { name: "Manpreet", sport: "Hockey" },
-//   { name: "Sunil", sport: "Football" },
-//   { name: "Gurinder", sport: "Volleyball" },
-// ];
-// //map the function
-// document.write(jsondata.map(NameDetail));
-// //create a function to take array values
-// function NameDetail(item) {
-//   data = [`${item.name} is captain of Indian ${item.sport} Team <br>`];
-//   document.write(data);
-// }
+// map implementation
+document.write(`<h2> map function </h2>`);
+let data;
+const jsondata6 = [
+  { name: "Manpreet", sport: "Hockey" },
+  { name: "Sunil", sport: "Football" },
+  { name: "Gurinder", sport: "Volleyball" },
+];
+//create a function to take array values
+let NameDetail = (item) => {
+  data = [`${item.name} is captain of Indian ${item.sport} Team <br>`];
+  document.write(data);
+};
+//map the function
+document.write(jsondata6.map(NameDetail));
+document.write(`<br>`);
--- switch(T-1).js
+++ switch(T-1).js
@@ -1,36 +1,37 @@
+// getting value from user
+let value = prompt(`Enter value :`);
+document.write(`value : ${value} <br>`);
 // created function to get value as number
 const dayNumber = (day) =>{
     value = Number(day);
 
 switch (value) {
   case 1:
-    document.write(`Sunday <br>`);
-    break;
-  case 2:
     document.write(`Monday <br>`);
     break;
-  case 3:
+  case 2:
     document.write(`Tuesday <br>`);
     break;
+  case 3:
+    document.write(`Wednessday <br>`);
+    break;
   case 4:
-    document.write(`Wednesday <br>`);
+    document.write(`Thursday <br>`);
     break;
   case 5:
-    document.write(`Thrusday <br>`);
-    break;
-  case 6:
     document.write(`Friday <br>`);
     break;
-  case 7:
+  case 6:
     document.write(`Saturday <br>`);
+    break;
+  case 7:
+    document.write(`Sunday <br>`);
     break;
   default:
     document.write(`Invalid input please check <br>`);
     break;
 }
 }
-// getting value from user
-let value = prompt(`Enter value :`);
-document.write(`value : ${value} <br>`);
+
 //calling function
 dayNumber(value);
